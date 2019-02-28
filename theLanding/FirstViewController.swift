bce9914241e539496aadfdd7d004756b15e9f459
Seth Childers <sethchilders92@gmail.com>
2019-02-27 21:13:30 -0700

    Committing changes

--- theLanding/FirstViewController.swift
+++ theLanding/FirstViewController.swift
@@ -27,6 +27,8 @@ class FirstViewController: UIViewController, CLLocationManagerDelegate {
     var userLocation:CLLocation!
     // distance to next stop
     var metersToShuttleStop: [CLLocationDistance]!
+    // the collection of shuttle location names and coordinates
+//    var locations: [(name: String, coordinates: CLLocation)]!
     // closest stop to the users current location
     var closestLocation: (name: String, location: CLLocation, distance: Double)!
     // walking time in minutes to closest shuttle stop
@@ -36,7 +38,7 @@ class FirstViewController: UIViewController, CLLocationManagerDelegate {
     // the shuttle schedule
     var schedule: [String]!
     // the database
-    let db = Firestore.firestore()
+    lazy var db = Firestore.firestore()
     // the map
     @IBOutlet weak var mapView: MKMapView!
     
@@ -151,86 +153,64 @@ class FirstViewController: UIViewController, CLLocationManagerDelegate {
         print("Error \(error)")
     }
     
-//    func getCoordinates() {
-//        DispatchQueue.global().async {
-//            self.db.collection("locations").document("coordinates").getDocument { (document, error) in
-//                if let error = error {
-//                    print("Error getting documents: \(error)")
-//                } else {
-//                    if let document = document, document.exists {
-//                        let coordinates = document.data()
-//                        print("coordinates: \(String(describing: coordinates))")
-//                        //                    for times in locationTimes! {
-////                            for time in times.value as! [String] {
-////                                myTimes.append(time)
-////                            }
-////                        }
-//                    }
-////                    DispatchQueue.main.async {
-////
-////                    }
-//                }
-//            }
-//        }
-//    }
-    
+    func getCoordinates() {
+
+    }
+
     // loop throught the locations and find the closest one to the user
     func getClosestLocation() {
         // we'll need to put these in firebase
-        let locations = [
-            ("MC", CLLocation.init(latitude: 43.817830, longitude: -111.784218)),
-            ("Landing", CLLocation.init(latitude: 43.817204, longitude: -111.794373)),
-            ("LLot", CLLocation.init(latitude: 43.811734, longitude: -111.782626)),
-            ("Romney", CLLocation.init(latitude: 43.820142, longitude: -111.784076)),
-            ("Walmart", CLLocation.init(latitude: 43.8560, longitude: -111.7739)),
-            ("Broulims", CLLocation.init(latitude: 43.827012, longitude: -111.787335)),
-        ]
-        
+//        let locations = [
+//            ("MC", CLLocation.init(latitude: 43.817830, longitude: -111.784218)),
+//            ("Landing", CLLocation.init(latitude: 43.817204, longitude: -111.794373)),
+//            ("LLot", CLLocation.init(latitude: 43.811734, longitude: -111.782626)),
+//            ("Romney", CLLocation.init(latitude: 43.820142, longitude: -111.784076)),
+//            ("Walmart", CLLocation.init(latitude: 43.8560, longitude: -111.7739)),
+//            ("Broulims", CLLocation.init(latitude: 43.827012, longitude: -111.787335)),
+//        ]
+
         DispatchQueue.global().async {
+            // get the 'coordinates' document from firebase with all the locations and their coordinates
             self.db.collection("locations").document("coordinates").getDocument { (document, error) in
-                var myTimes: [String] = []
                 if let error = error {
                     print("Error getting documents: \(error)")
                 } else {
                     if let document = document, document.exists {
-                        let locationTimes = document.data()
-                        for times in locationTimes! {
-                            for time in times.value as! [String] {
-                                myTimes.append(time)
+                        // a variable to store the formatted locations in
+                        var locations = [(name: String, coordinates: CLLocation)]()
+                        // get the data from the document
+                        let locationsData = document.data()
+                        // for each item in the document, format it and append it to the 'locations' variable
+                        for location in locationsData! {
+                            let coord = location.value as! GeoPoint
+                            let lat = coord.latitude
+                            let lon = coord.longitude
+                            locations.append((name: location.key,
+                                                  coordinates: CLLocation.init(latitude: lat, longitude: lon)))
+                        }
+                        // make a random location that is far, far away
+                        var tempClosest = ("No where", CLLocation.init(latitude: 24.8560, longitude: -12.7739), 1000000000.00)
+                        for shuttleStop in locations {
+                            // calculate the distance to the shuttle location from the user's current location
+                            let distance = [(self.userLocation? .distance(from: shuttleStop.1))!]
+                            // if the location being checked is closer, assign it as the closest
+                            if (distance[0] < tempClosest.2) {
+                                // (shuttle stop name, coordinates of the stop, how far to the stop)
+                                tempClosest = (shuttleStop.0, shuttleStop.1, distance[0])
                             }
                         }
-                    }
-                    DispatchQueue.main.async {
-                        
+                        // set the member variable
+                        self.closestLocation = tempClosest
+                        print("Closest Location: \(String(describing: self.closestLocation))")
+                        // after finding the closest location, calculate walking time
+                        self.getWalkingTime()
                     }
                 }
             }
-            // first make a random location that is far, far away
-            var tempClosest = ("No where", CLLocation.init(latitude: 24.8560, longitude: -12.7739), 1000000000.00)
-            for shuttleStop in locations {
-                // calculate the distance to the shuttle location from the user's current location
-                let distance = [(self.userLocation? .distance(from: shuttleStop.1))!]
-                // if the location being checked is closer, assign it as the closest
-                if (distance[0] < tempClosest.2) {
-                    // (shuttle stop name, coordinates of the stop, how far to the stop)
-                    tempClosest = (shuttleStop.0, shuttleStop.1, distance[0])
-                }
-            }
-//            DispatchQueue.main.async {
-                // set the member variable
-                self.closestLocation = tempClosest
-                print(self.closestLocation)
-                // after finding the closest location, calculate walking time
-                self.getWalkingTime()
-//            }
         }
     }
     
-    // Map rendering
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        findCurrentLocation()
-    }
+
     /* These next to methods are dependent on each other. Code them carefully. */
     
     // Based on the users location, calculate the walking time to the closest shuttle stop.
@@ -254,7 +234,7 @@ class FirstViewController: UIViewController, CLLocationManagerDelegate {
                 self.travelTime = (response?.expectedTravelTime)! / 60.00
                 print("ETA: \(String(describing: self.travelTime))")
             } else {
-                print("Error!")
+                print("Error calculating ETA!")
             }
         })
     }
